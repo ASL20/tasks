9a504c40d6c92e5b1d6ef02e72a50ab49f2c3422
Anton Lipich <anton.lipich1@gmail.com>
2021-05-07 11:47:18 +0300

    3 commit

--- 06_05/23.rb
+++ 06_05/23.rb
@@ -7,4 +7,39 @@ while index <= number
   index += 1
 end
 
-# arr.each { |num| puts "* " * num }
+arr = [1, 2, 3, 4, 5]
+
+index = 1
+
+arr.each do |num|
+  print " " * ((arr[-num]) - 1)
+  puts "* " * index
+  index += 1
+end
+
+index = 0
+
+while index < arr.size
+  count = 0
+  print " " * (arr.size - 1 - index)
+  while count <= index
+    print "#{arr[count]} "
+    count += 1
+  end
+  puts
+
+  index += 1
+end
+
+index = 0
+
+arr.each do |num|
+  count = 0
+  print " " * ((arr[-num]) - 1)
+  while count <= index
+    print "#{arr[count]} "
+    count += 1
+  end
+  puts
+  index += 1
+end

5fd7b9b28768ab905f2e6a5f00106b086ebe74a0
Anton Lipich <anton.lipich1@gmail.com>
2021-05-11 14:30:58 +0300

    refactor 30.rb

--- 06_05/30.rb
+++ 06_05/30.rb
@@ -1,22 +1,57 @@
-number = rand(100) + 1
-attempts = 0
-puts number
-while attempts < 3
-  puts 'Введите число от 1 до 100'
-  input = STDIN.gets.to_i
+require 'minitest/autorun'
 
-  attempts += 1
-  if input > number
-    puts 'Нужно меньше'
-  elsif input < number
-    puts 'Нужно больше'
+def compare_numbers(conceived, answer)
+  if answer > conceived
+    'Нужно меньше'
+  elsif answer < conceived
+    'Нужно больше'
   else
-    break
+    nil
   end
 end
 
-if attempts == 3
-  puts "Вы проиграли. Это было число: #{number}"
-elsif
-  puts "Вы выиграли\nПотрачено попыток: #{attempts}"
+def print_result(number, attempts)
+  if attempts == 3
+    "Вы проиграли. Это было число: #{number}"
+  else
+    "Вы выиграли\nПотрачено попыток: #{attempts}"
+  end
+end
+
+number = rand(100) + 1
+attempts = 0
+
+while attempts < 3
+  attempts += 1
+
+  puts 'Введите число от 1 до 100'
+  input = STDIN.gets.to_i
+
+  if compare_numbers(number, input).nil?
+    break
+  else
+    puts compare_numbers(number, input)
+  end
+end
+
+class Test < Minitest::Test
+  def test_compare_numbers_hidden_number_is_greater
+    assert_equal('Нужно больше', compare_numbers(50, 49))
+  end
+
+  def test_compare_numbers_hidden_number_is_less
+    assert_equal('Нужно меньше', compare_numbers(49, 50))
+  end
+
+  def test_compare_numbers_hidden_number_equal
+    assert_nil(compare_numbers(50, 50))
+  end
+
+  def test_print_result_won
+    assert_equal("Вы проиграли. Это было число: #{50}", print_result(50, 3))
+  end
+
+  def test_print_result_lost
+    assert_equal("Вы выиграли\nПотрачено попыток: #{2}", print_result(50, 2))
+  end
 end

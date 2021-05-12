6a28027b83b8d725b559eb14511fae146ebb2c92
Anton Lipich <anton.lipich1@gmail.com>
2021-05-12 18:59:44 +0300

    fix 18

--- 07_05/18.rb
+++ 07_05/18.rb
@@ -3,19 +3,30 @@ require 'minitest/autorun'
 
 def subtract_arrays(arr1, arr2)
   return unless arr1.is_a?(Array) || arr2.is_a?(Array)
-
+  return arr1 if arr1.length == 0
   result = []
-  index = 0
-  arr1.each do |n|
-    result << n if n != arr2[index]
-    index += 1 if n == arr2[index]
+  result[0] = arr1
+  arr2.each do |s|
+    tmp = []
+    index = 0
+    n = 0
+    result[index].each do |f|
+      if n == 0
+        tmp << f if f != s
+      else
+        tmp << f
+      end
+      n += 1 if f == s
+    end
+    result[index] = tmp
+    index += 1
   end
-  result
+  result.last
 end
 
 class Test < Minitest::Test
   def test_subtract_arrays_correct_work
-    assert_equal([3, 4, 5, 6], subtract_arrays([1, 2, 3, 4, 5, 6], [1, 2]))
+    assert_equal([4, 5, 6], subtract_arrays([1, 2, 3, 4, 5, 6], [2, 1, 3]))
   end
 
   def test_subtract_arrays_wrong_input

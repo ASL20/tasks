edf58f7be12a20a3af14d3cfe533b3337a69d29c
Anton Lipich <anton.lipich1@gmail.com>
2021-05-18 11:55:42 +0300

    fix 2 players game

--- roulette/main.rb
+++ roulette/main.rb
@@ -22,6 +22,18 @@ def check_bet(player_choice, game_combination)
   end
 end
 
+def place_bet(balance)
+  puts 'Введите сумму ставки (минимальная сумма ставки - 10)'
+  player_bet_value = STDIN.gets.to_i
+
+  if player_bet_value < 10
+    puts "Вы хотели поставить #{player_bet_value}. Нужно поставить больше"
+  elsif (balance - player_bet_value) < 0
+    puts "Вы хотели поставить #{player_bet_value}. У вас нет столько денег, попробуйте поставить меньше"
+  end
+  player_bet_value
+end
+
 def print_balance(balance)
   puts "Ваш баланс #{balance}"
 end
@@ -62,9 +74,9 @@ def print_combination(combination)
   puts "Выпало число: #{combination[0]} #{combination[1]}."
 end
 
-def print_move_result(result, user_choice, bet_value)
-  puts result ? "Вы поставили на '#{user_choice}' #{bet_value} - ставка выиграла" :
-                "Вы поставили на '#{user_choice}' #{bet_value} - ставка проиграла"
+def print_move_result(result, user_choice, bet_value, player)
+  puts result ? "#{player} поставил на '#{user_choice}' #{bet_value} - ставка выиграла" :
+                "#{player} поставил на '#{user_choice}' #{bet_value} - ставка проиграла"
 end
 
 def print_pc_move_result(result, pc_choice, pc_bet_value)
@@ -72,7 +84,7 @@ def print_pc_move_result(result, pc_choice, pc_bet_value)
                 "ПК поставил на '#{pc_choice}' #{pc_bet_value} - ставка проиграла"
 end
 
-def print_status(balance, pc_balance)
+def print_status
   <<~STATUS
     ---------------------------
     Выберите действие:
@@ -119,7 +131,7 @@ numbers = [
   9, 22, 18, 29, 7, 28, 12, 35, 3, 26
 ]
 
-balance = 1000
+player1_balance = 1000
 player2_balance = 1000
 pc_balance = 1000
 status = true
@@ -134,97 +146,111 @@ puts <<~MESSAGE
      MESSAGE
 game_selection = STDIN.gets.to_i
 
-while status || balance > 10
-  input = 0
-  player_choice = -1
-  bet_value = 0
+while status || player1_balance > 10
+  player1_input = 0
+  player2_input = 0
+  player1_choice = -1
+  player1_bet_value = 0
   player2_bet_value = 0
-  bet_type = 0
+  player1_bet_type = 0
   player2_bet_type = 0
   pc_choice = [numbers.sample, 'red', 'black', 'even', 'odd'].sample if game_selection == 2
   pc_bet_value = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100].sample if game_selection == 2
 
   # выбираем действие игрока
-  puts print_status(balance, pc_balance)
-  until input.between?(1, 4)
-    input = STDIN.gets.to_i
+  puts print_status
+  until player1_input.between?(1, 4)
+    player1_input = STDIN.gets.to_i
   end
 
-  case input
-  when 1
-    until bet_type.between?(1, 3)
-      puts print_bet_type
-      bet_type = STDIN.gets.to_i
+  if game_selection == 3
+    puts print_status
+    until player2_input.between?(1, 4)
+      player2_input = STDIN.gets.to_i
     end
+  end
 
-    player_choice = print_choice_menu(bet_type)
-
-    while bet_value < 10 || (balance - bet_value) < 0
-      puts 'Введите сумму ставки (минимальная сумма ставки - 10)'
-      bet_value = STDIN.gets.to_i
-
-      if bet_value < 10
-        puts "Вы хотели поставить #{bet_value}. Нужно поставить больше"
-      elsif (balance - bet_value) < 0
-        puts "Вы хотели поставить #{bet_value}. У вас нет столько денег, попробуйте поставить меньше"
-      end
-    end
-
-    until player2_bet_type.between?(1, 3)
-      puts print_bet_type
-      player2_bet_type = STDIN.gets.to_i
-    end
-
-    player2_choice = print_choice_menu(player2_bet_type)
-
-    while player2_bet_value < 10 || (balance - player2_bet_value) < 0
-      puts 'Введите сумму ставки (минимальная сумма ставки - 10)'
-      player2_bet_value = STDIN.gets.to_i
-
-      if player2_bet_value < 10
-        puts "Вы хотели поставить #{player2_bet_value}. Нужно поставить больше"
-      elsif (balance - player2_bet_value) < 0
-        puts "Вы хотели поставить #{player2_bet_value}. У вас нет столько денег, попробуйте поставить меньше"
-      end
-    end
-
+  if player1_input == 4 && player2_input == 4 ||
+     player1_input == 4 && game_selection == 1 ||
+     player1_input == 4 && game_selection == 2
+    break
+  elsif player1_input == 3 && player2_input == 3
+    print_balance(player1_balance)
+    print_balance(player2_balance)
+  elsif player1_input == 3
+    print_balance(player1_balance)
+  elsif player2_input == 3
+    print_balance(player2_balance)
+  elsif player1_input == 2 && player2_input == 2 ||
+        player1_input == 2 && game_selection == 1 ||
+        player1_input == 2 && game_selection == 2
     rotate_roulette
 
     dropped_number = select_number(numbers)
     dropped_combination = select_color(dropped_number)
-    player_result = check_bet(player_choice, dropped_combination)
-    player2_result = check_bet(player2_choice, dropped_combination) if game_selection == 3
+    print_combination(dropped_combination)
+  elsif player1_input == 1 || player2_input == 1
+    if player1_input == 1
+      until player1_bet_type.between?(1, 3)
+        puts print_bet_type
+        player1_bet_type = STDIN.gets.to_i
+      end
+
+      #выбираем на что поставить 1 игроку
+      player1_choice = print_choice_menu(player1_bet_type)
+
+      while player1_bet_value < 10 || (player1_balance - player1_bet_value) < 0
+        player1_bet_value = place_bet(player1_balance)
+      end
+    end
+
+    if player2_input == 1
+      until player2_bet_type.between?(1, 3)
+        puts print_bet_type
+        player2_bet_type = STDIN.gets.to_i
+      end
+
+      # Выбираем на что поставить 2 игроку
+      player2_choice = print_choice_menu(player2_bet_type)
+
+      while player2_bet_value < 10 || (player2_balance - player2_bet_value) < 0
+        player2_bet_value = place_bet(player2_balance)
+      end
+    end
+
+    # имитируем вращение рулетки
+    rotate_roulette
+
+    # создаем выпавшую комбинацию
+    dropped_number = select_number(numbers)
+    dropped_combination = select_color(dropped_number)
+
+    # проверяем - сыграла ставка или нет
+    player1_result = check_bet(player1_choice, dropped_combination) if player1_input == 1
+    player2_result = check_bet(player2_choice, dropped_combination) if player2_input == 1
     pc_result = check_bet(pc_choice, dropped_combination) if game_selection == 2
 
+    # выводим выпавшую комбинацию и результат ставки
     print_combination(dropped_combination)
-    print_move_result(player_result, player_choice, bet_value)
-    print_move_result(player2_result, player2_choice, player2_bet_value) if game_selection == 3
+    print_move_result(player1_result, player1_choice, player1_bet_value, 'player1') if player1_input == 1
+    print_move_result(player2_result, player2_choice, player2_bet_value, 'player2') if player2_input == 1
     print_pc_move_result(pc_result, pc_choice, pc_bet_value) if pc_status && game_selection == 2
 
-    puts "Выигрыш 1 игрока составляет #{calculate_balance(bet_value, player_result, player_choice)}" if player_result
-    puts "Выигрыш 2 игрока составляет #{calculate_balance(player2_bet_value, player2_result, player2_choice)}" if player2_result && game_selection == 3
+    puts "Выигрыш 1 игрока составляет #{calculate_balance(player1_bet_value, player1_result, player1_choice)}" if player1_result
+    puts "Выигрыш 2 игрока составляет #{calculate_balance(player2_bet_value, player2_result, player2_choice)}" if player2_result && player2_input == 1
     puts "Выигрыш ПК составляет #{calculate_balance(pc_bet_value, pc_result, pc_choice)}" if pc_result && pc_status && game_selection == 2
-    balance += calculate_balance(bet_value, player_result, player_choice)
-    player2_balance += calculate_balance(player2_bet_value, player2_result, player2_choice) if game_selection == 3
+
+    player1_balance += calculate_balance(player1_bet_value, player1_result, player1_choice)
+    player2_balance += calculate_balance(player2_bet_value, player2_result, player2_choice) if player2_input == 1
     pc_balance += calculate_balance(pc_bet_value, pc_result, pc_choice) if pc_status && game_selection == 2
 
-    puts "Баланс 1 игрока #{balance}"
+    puts "Баланс 1 игрока #{player1_balance}"
     puts "Баланс 2 игрока #{player2_balance}" if game_selection == 3
     puts "Баланс ПК #{pc_balance}" if pc_status && game_selection == 2
 
     pc_status = false if pc_balance < 10 && game_selection == 2
-    break if balance < 10
-  when 2
-    rotate_roulette
-
-    dropped_number = select_number(numbers)
-    dropped_combination = select_color(dropped_number)
-    print_combination(dropped_combination)
-  when 3
-    print_balance(balance)
-  when 4
-    break
+    break if player1_balance < 10
   end
 end
 
-summarize(balance)
+summarize(player1_balance)
